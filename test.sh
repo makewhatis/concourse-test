a368e23d5f324e0d2ba9ca8868f1db941a07e1eb
David Johansen <djohansen@fitbit.com>
2016-03-03 11:07:24 -0800

    boo

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@
 apt-get update
 apt-get install -y rake
 
+wget -qO- https://get.docker.com/ | sh
+apt-get -y install python-pip
+pip install docker-compose
+
 cd concourse-docker-compose
 rake # wait awhile, grab a coffee
 cd example/
