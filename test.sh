a8f5fc224c0caaf36b8fe7450f12689e7530d436
David Johansen <djohansen@fitbit.com>
2016-03-03 10:49:36 -0800

    boo

--- test.sh
+++ test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-ls -l
-pwd
-
-ls -l /
-
-ls -l /var
-
-cat /etc/passwd
-
-nc -vz localhost 7777
+cd ../concourse-docker-compose
+rake # wait awhile, grab a coffee
+cd example/
+docker-compose build
+docker-compose up
